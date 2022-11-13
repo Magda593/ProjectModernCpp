8102125b2e646321197e2f72d10f05bfe67adf77
Magda_unitbv <magdalena.munteanu@student.unitbv.ro>
2022-11-13 22:35:48 +0200

    Created the class for questions of type 2

---
+++ Triviador/QuestionType2.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include<unordered_map>
+
+class QuestionType2
+{
+public:
+
+
+private:
+	std::string m_question2;
+	int m_answer2;
+	std::unordered_map<std::string, int> m_questionType2;
+};
+
