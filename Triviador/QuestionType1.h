08716a78ab04728565075c1c6511498d09c6849b
Magda_unitbv <magdalena.munteanu@student.unitbv.ro>
2022-11-13 22:28:16 +0200

    Crated the class for questions of type 1; plus it's members

---
+++ Triviador/QuestionType1.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<unordered_map>
+#include<vector>
+#include<iostream>
+
+class QuestionType1
+{
+public:
+
+
+private:
+	std::string m_question1;
+	std::vector<std::string> m_answer1;
+	std::unordered_map<std::string, std::vector<std::string>> m_questionType1;
+};
+
