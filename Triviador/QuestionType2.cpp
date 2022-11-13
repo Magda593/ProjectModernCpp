b4684f8bd42b4bd69b31e33376b9690bf23a9148
Magda_unitbv <magdalena.munteanu@student.unitbv.ro>
2022-11-13 22:40:42 +0200

    Wrote the constructors for the class and created a SaveQuestions method.

--- Triviador/QuestionType2.cpp
+++ Triviador/QuestionType2.cpp
@@ -1,1 +1,16 @@
 #include "QuestionType2.h"
+
+QuestionType2::QuestionType2()
+{
+}
+
+QuestionType2::QuestionType2(const std::string& question2, int answer):
+	m_question2(question2),
+	m_answer(answer)
+{
+}
+
+void QuestionType2::SaveQuestion(std::string question2, int answer)
+{
+	m_questionType2.insert(std::make_pair(question2, answer));
+}
--- Triviador/QuestionType2.h
+++ Triviador/QuestionType2.h
@@ -5,11 +5,14 @@
 class QuestionType2
 {
 public:
+	QuestionType2();
+	QuestionType2(const std::string& question2, int answer);
 
+	void SaveQuestion(std::string question2, int answer);
 
 private:
 	std::string m_question2;
-	int m_answer2;
+	int m_answer;
 	std::unordered_map<std::string, int> m_questionType2;
 };
 
