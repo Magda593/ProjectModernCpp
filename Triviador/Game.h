5bcca1c6ba3f239db0c0390cb283abed74a72705
IoanaMareniuc <115571780+IoanaMareniuc@users.noreply.github.com>
2022-12-04 11:17:47 +0200

    Created the class Game and added it's members

---
+++ Triviador/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "AccountManager.h"
+#include "QuestionType1.h"
+#include "QuestionType2.h"
+#include "Board.h"
+
+class Game
+{
+public:
+	void MenuForTheGame();
+
+private:
+	AccountManager user;
+	Board board;
+	QuestionType1 question1;
+	QuestionType2 question2;
+	int m_option;
+};
