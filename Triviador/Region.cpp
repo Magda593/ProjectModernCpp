4456b62335bf2a62be942219171ff82d364f053b
Magda_unitbv <magdalena.munteanu@student.unitbv.ro>
2022-12-11 13:31:53 +0200

    Changed the enums; created a simple constructor

--- Triviador/Region.cpp
+++ Triviador/Region.cpp
@@ -1,8 +1,16 @@
 #include "Region.h"
 
+Region::Region() :
+	Region(Base::None, SimpleRegion::None, UpgradedRegion::None)
+{
+	//Empty
+}
+
 Region::Region(Base base, SimpleRegion simpleRegion, UpgradedRegion upgradedRegion) :
 	m_base(base),
 	m_simpleRegion(simpleRegion),
 	m_upgradedRegion(upgradedRegion)
 {
+	//Empty
 }
+
--- Triviador/Region.h
+++ Triviador/Region.h
@@ -4,20 +4,26 @@ class Region
 public:
 	enum class Base : int
 	{
+		None,
 		Base
 	};
 	enum class SimpleRegion : int
 	{
+		None,
 		SimpleRegion
 	};
 	enum class UpgradedRegion : int
 	{
+		None,
 		UpgradedRegion
 	};
 
 public:
+	Region();
 	Region(Base base, SimpleRegion simpleRegion, UpgradedRegion upgradedRegion);
 
+	Region& operator = (const Region& other);
+	Region& operator = (Region&& other);
 
 private:
 	Base m_base : 3;
