fb31e902f3b25ea7668a0102a2cb34fc1387d7ec
Magda_unitbv <magdalena.munteanu@student.unitbv.ro>
2022-12-11 13:11:01 +0200

    Created the class Region and added the enum for base, simpleRegion and upgradedRegion

---
+++ Triviador/Region.h
@@ -0,0 +1,18 @@
+#pragma once
+class Region
+{
+public:
+	enum class Base : int
+	{
+		Base
+	};
+	enum class SimpleRegion : int
+	{
+		SimpleRegion
+	};
+	enum class UpgradedRegion : int
+	{
+		UpgradedRegion
+	};
+};
+
