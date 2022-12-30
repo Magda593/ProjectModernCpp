6b33b3137426021bfd7f0ba5f0c9e32565ab941f
IoanaMareniuc <115571780+IoanaMareniuc@users.noreply.github.com>
2022-12-30 21:21:25 +0200

    Created a method to get a random number between 0 and x to get random questions

--- Triviador/QuestionManager.cpp
+++ Triviador/QuestionManager.cpp
@@ -1,1 +1,9 @@
 #include "QuestionManager.h"
+
+int QuestionManager::GetRandomNumber(int x)
+{
+	srand(time(NULL));
+	int res;
+	res= rand() % x + 0;
+	return res;
+}
--- Triviador/QuestionManager.h
+++ Triviador/QuestionManager.h
@@ -1,5 +1,14 @@
 #pragma once
+
+#include <stdlib.h>
+#include <time.h>
+//#include <GrillQuestion.h>
+//#include <QuestionType2.h>
+
 class QuestionManager
 {
+public:
+
+	int GetRandomNumber(int x);
 };
 
