f2738bcca8f88928258c3697568a3a83ffac8b0e
Magda_unitbv <magdalena.munteanu@student.unitbv.ro>
2023-01-10 19:45:56 +0200

    Comments received at lab for several things

--- Triviador/Source.cpp
+++ Triviador/Source.cpp
@@ -11,6 +11,28 @@
 
 int main()
 {
+	std::unordered_map<std::string, int> m_questionType2;
+	m_questionType2.insert({ "a",5 });
+	m_questionType2.insert({ "b",5 });
+	m_questionType2.insert({ "c",5 });
+	
+	std::pair<std::string, int> p;
+	auto x = m_questionType2.begin();
+	auto y = m_questionType2.end();
+
+	int poz = 2;
+	int nr = 0;
+	for (auto el : m_questionType2)
+	{
+		if (nr == poz)
+		{
+			p = el;
+			break;
+		}
+		nr++;
+	}
+
+
 	system("Color 0A"); //light green
 	Board board;
 	int numberOfPlayers;
@@ -18,9 +40,41 @@ int main()
 	board.SetNumberOfPlayers(numberOfPlayers);
 	board.SetBoard();
 	std::cout << "Empty board:\n" << board << std::endl; 
-	board[{0, 1}] = Region::Regions::SimpleRegion;
-	std::cout << board;
-
+	while (!board.IsFull())
+	{
+		int line, column;
+		std::cin >> line >> column;
+		if (!board[{line, column}])
+			board[{line, column}] = Region::Regions::SimpleRegion;
+		else
+			std::cout << "Already there";
+		
+		std::cout << "\n";
+		std::cout << board;
+		std::cout << "\n";
+	}
+	/*board[{0, 1}] = Region::Regions::SimpleRegion;
+	std::cout << board;*/
 
 	return 0;
 }
+
+/*
+1. fie scriu colorat in consola 
+2. ori initiala user si scor
+
+Raspund la aceiasi intrebare, 
+daca raspund la fel se mai pune o intrebare pana raspund diferit 
+
+map+20
+sau?
+for auto iterator de la map.begin; diferit de iterator +20 
+
+sa ne interesam de chrono pentru timp 
+
+regex login
+regex register
+
+
+
+*/
